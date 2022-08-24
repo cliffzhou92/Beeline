c7fe1c023eef634b8e8e484030502efec8ab3eca
cliffzhou92 <46710747+cliffzhou92@users.noreply.github.com>
2022-08-23 17:33:09 -0700

    update spliceJAC input

--- run_bifur.sh
+++ run_bifur.sh
@@ -3,8 +3,11 @@
 # Set-up Anaconda virtual environment
 python BLRunner.py --config config-files/config_bl_bifur_0.yaml
 python BLRunner.py --config config-files/config_bl_bifur_1.yaml
+python BLRunner.py --config config-files/config_bl_bifur_2.yaml
 
 python BLEvaluator.py --config config-files/config_bl_bifur_0.yaml \
                       --a --j --e --spearman --sepr --m
 python BLEvaluator.py --config config-files/config_bl_bifur_1.yaml \
+                      --a --j --e --spearman --sepr --m
+python BLEvaluator.py --config config-files/config_bl_bifur_2.yaml \
                       --a --j --e --spearman --sepr --m
